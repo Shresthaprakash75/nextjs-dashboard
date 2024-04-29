c5c989918231bedc5216056288a7399c3c8c3bec
prakash <light@prakashs-MacBook-Air.local>
2024-04-29 14:33:16 +0545

    another fuck you

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,7 +2,7 @@ import { Inter, Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
  
-export const lusitana = Lusitana({
+export const iusitana = Lusitana({
   weight: ['400', '700'],
   subsets: ['latin'],
 });
